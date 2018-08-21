e33e627a6ac3f08a1f90920539938883faeceab8
redeyes2015 <redeyes2015@gmail.com>
2018-08-21 18:38:18 +0800

    fish: update abbr

--- abbr.fish
+++ abbr.fish
@@ -2,9 +2,13 @@
 abbr m make
 abbr vi vim
 abbr g git
-abbr glg 'git lg'
-abbr tmux 'tmux -2'
-abbr gst 'git status'
-abbr gst. 'git status .'
+abbr gcfix 'git ci --fixup'
+abbr gci 'git commit -v'
+abbr gcia 'git ci --amend'
+abbr gco 'git checkout'
 abbr gd 'git dsf'
-abbr gco 'git co'
+abbr gf 'git fetch'
+abbr ggp 'git grep'
+abbr glg 'git lg'
+abbr gst 'git status'
+abbr tmux 'tmux -2'
