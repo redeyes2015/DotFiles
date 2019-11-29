d4fbbcaca77b0f6bc225ce2881b2886e20a7074d
redeyes2015 <redeyes0215@gmail.com>
2019-11-29 11:49:13 +0800

    Make shellcheck happy

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,28 +4,28 @@ InstallDir=$HOME/DotFiles
 
 curl -sSLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s $InstallDir/vimrc ~/.vimrc
-ln -s $InstallDir/gitconfig ~/.gitconfig
+ln -s "$InstallDir/vimrc" ~/.vimrc
+ln -s "$InstallDir/gitconfig" ~/.gitconfig
 tmpvimrc=$(mktemp)
 sed -n '1,/plug\#end/p' ~/.vimrc > "$tmpvimrc"
 vim -u "$tmpvimrc" +PlugInstall +qa
 rm "$tmpvimrc"
 
 mkdir -p ~/.config/fish
-ln -s $InstallDir/config.fish ~/.config/fish/config.fish
-fish < $InstallDir/abbr.fish
+ln -s "$InstallDir/config.fish" ~/.config/fish/config.fish
+fish < "$InstallDir/abbr.fish"
 curl -sSLo ~/.config/fish/functions/fisher.fish --create-dirs \
     https://raw.githubusercontent.com/jorgebucaran/fisher/master/fisher.fish
-ln -s $InstallDir/fishfile ~/.config/fish/fishfile
+ln -s "$InstallDir/fishfile" ~/.config/fish/fishfile
 fish -c fisher
 
-find $InstallDir/fish-functions/ -type f -name '*.fish' -print0\
+find "$InstallDir/fish-functions/" -type f -name '*.fish' -print0\
     | xargs -0 -I FILE ln -s FILE ~/.config/fish/functions/
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install --no-zsh --no-bash
 
-git clone --depth 1 https://github.com/so-fancy/diff-so-fancy $InstallDir/diff-so-fancy.repo
+git clone --depth 1 https://github.com/so-fancy/diff-so-fancy "$InstallDir/diff-so-fancy.repo"
 
 git clone --depth 1 https://github.com/creationix/nvm.git ~/.nvm
 
