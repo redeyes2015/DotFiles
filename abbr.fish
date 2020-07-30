9b9b01ea2ce64fa00cd716a0eadd770d2e075855
redeyes2015 <redeyes0215@gmail.com>
2020-07-30 18:28:07 +0800

    Update abbr.fish

--- abbr.fish
+++ abbr.fish
@@ -1,14 +1,18 @@
-#!/usr/bin/fish
-abbr m make
-abbr vi vim
-abbr g git
-abbr gcfix 'git ci --fixup'
-abbr gci 'git commit -v'
-abbr gcia 'git ci --amend'
-abbr gco 'git checkout'
-abbr gd 'git dsf'
-abbr gf 'git fetch'
-abbr ggp 'git grep'
-abbr glg 'git lg'
-abbr gst 'git status'
-abbr tmux 'tmux -2'
+abbr -a -U -- g git
+abbr -a -U -- gap 'git add -p'
+abbr -a -U -- gau 'git add -u'
+abbr -a -U -- gcb 'git checkout -b'
+abbr -a -U -- gcfix 'git ci --fixup'
+abbr -a -U -- gci 'git commit -v'
+abbr -a -U -- gcia 'git ci --amend'
+abbr -a -U -- gco 'git checkout'
+abbr -a -U -- gd 'git dsf'
+abbr -a -U -- gf 'git fetch'
+abbr -a -U -- ggp 'git grep'
+abbr -a -U -- glg 'git lg'
+abbr -a -U -- grb 'git rebase'
+abbr -a -U -- grbi 'git rebase -i'
+abbr -a -U -- gst 'git status'
+abbr -a -U -- m make
+abbr -a -U -- tmux 'tmux -2'
+abbr -a -U -- vi vim
