7e9bbe37740b5cfe2a958ec7caf68e52e6c234c5
redeyes2015 <redeyes0215@gmail.com>
2021-11-03 12:14:38 +0800

    Update abbr.fish, notably added `gcfix`

--- abbr.fish
+++ abbr.fish
@@ -1,18 +1,25 @@
 abbr -a -U -- g git
+abbr -a -U -- ga 'git add'
 abbr -a -U -- gap 'git add -p'
 abbr -a -U -- gau 'git add -u'
-abbr -a -U -- gcb 'git checkout -b'
-abbr -a -U -- gcfix 'git ci --fixup'
+abbr -a -U -- gcfix git\ ci\ --fixup\ \(git\ log\ --oneline\ -n\ 100\ \|\ fzf\ --height\ 10\ \|\ awk\ \'\{print\ \$1\}\'\)
 abbr -a -U -- gci 'git commit -v'
 abbr -a -U -- gcia 'git ci --amend'
 abbr -a -U -- gco 'git checkout'
+abbr -a -U -- gcp 'git cherry-pick'
 abbr -a -U -- gd 'git dsf'
+abbr -a -U -- gdsg 'git dsf --staged'
 abbr -a -U -- gf 'git fetch'
 abbr -a -U -- ggp 'git grep'
 abbr -a -U -- glg 'git lg'
+abbr -a -U -- gpoc 'git push origin --force-with-lease (git branch --show-current)'
+abbr -a -U -- gpof 'git push origin --force-with-lease HEAD:'
 abbr -a -U -- grb 'git rebase'
+abbr -a -U -- grba 'git rebase --abort'
+abbr -a -U -- grbc 'git rebase --continue'
 abbr -a -U -- grbi 'git rebase -i'
 abbr -a -U -- gst 'git status'
+abbr -a -U -- k kubectl
 abbr -a -U -- m make
 abbr -a -U -- tmux 'tmux -2'
 abbr -a -U -- vi vim
