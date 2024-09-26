121602397e28d18fbc17db12b5ab3bd73c011386
redeyes2015 <redeyes0215@gmail.com>
2024-09-26 11:18:51 +0800

    Update ll.fish to fit MacOS version

--- fish-functions/ll.fish
+++ fish-functions/ll.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.toAsTB/ll.fish @ line 2
 function ll --description 'List contents of directory using long format'
-	ls -lh --time-style=long-iso $argv
+#	ls -lh --time-style=long-iso $argv
+	ls -loh -D "%F %R" $argv
 end
